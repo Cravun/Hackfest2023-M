c2827ccafa2c15b39f5fbd88eefbcc4f50016409
Craven <KrenntKB@yahoo.com>
2023-04-17 23:06:22 +0800

    Sign in

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,161 +1,92 @@
-import React from "react";
-import {
-  TextInput,
-  StyleSheet,
-  Text,
-  Linking,
-  Button,
-  View,
-} from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 
-export default function SignIn() {
+const SignIn = () => {
+  const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  const [InputValues, setInputValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (name, value) => {
+    setInputValues({
+      ...InputValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (!EMAIL_REGEX.test(InputValues.email)) {
+      Alert.alert("Invalid Email", "Please enter a valid email address");
+      return;
+    }
+    if (!PASSWORD_REGEX.test(InputValues.password)) {
+      Alert.alert(
+        "Invalid Password",
+        "Password must be at least 8 characters long and contain at least one letter and one number"
+      );
+      return;
+    }
+    Alert.alert("Success", "You have successfully logged in");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={[styles1.setFontSizeThree, { fontWeight: "bold" }]}>
-        {" "}
-        SIGN IN{" "}
-      </Text>
-
-      <Text style={{ paddingTop: 5, opacity: 0.5 }}>Welcome back! </Text>
-      <Text style={{ opacity: 0.5, paddingBottom: 10 }}>
-        {" "}
-        You've been missed!{" "}
-      </Text>
-
-      <View style={{ margin: 5 }}>
-        <Text> Email address: </Text>
+      <Text style={styles.text2}>Welcome back! </Text>
+      <View>
+        <Text style={styles.text1}>Email</Text>
         <TextInput
-          style={{
-            height: 40,
-            width: 300,
-            borderColor: "gray",
-            borderWidth: 0.5,
-          }}
-          underlineColorAndroid="transparent"
+          style={styles.input}
+          placeholder=""
+          onChangeText={(text) => handleInputChange("email", text)}
+          value={InputValues.email}
+          keyboardType="email-address"
+          autoCapitalize="words"
         />
-
-        <Text style={{ paddingTop: 5 }}> Password: </Text>
+      </View>
+      <View>
+        <Text style={styles.text1}>Password</Text>
         <TextInput
-          style={{
-            height: 40,
-            width: 300,
-            borderColor: "gray",
-            borderWidth: 0.5,
-          }}
+          style={styles.input}
+          placeholder=""
+          onChangeText={(text) => handleInputChange("password", text)}
+          value={InputValues.password}
           secureTextEntry={true}
-          underlineColorAndroid="transparent"
         />
       </View>
 
-      <View style={{ width: "80%" }}>
-        <Text
-          style={{
-            paddingTop: 2,
-            paddingBottom: 20,
-            color: "gray",
-            textAlign: "right",
-          }}
-          onPress={() => Linking.openURL("https://www.google.com/")}
-        >
-          Forgot Password{" "}
-        </Text>
-      </View>
-
-      <View
-        style={[
-          {
-            width: "70%",
-            margin: 10,
-            backgroundColor: "red",
-            alignContent: "center",
-          },
-        ]}
-      >
-        <Button onPress={this.buttonClickListener} title="LOGIN" color="red" />
-      </View>
-
-      <Text style={{ opacity: 0.5 }}>
-        ---------- Or continue with ----------
-      </Text>
-
-      <View
-        style={[
-          {
-            width: "70%",
-            margin: 10,
-            backgroundColor: "gray",
-            alignContent: "center",
-          },
-        ]}
-      >
-        <Button
-          onPress={this.buttonClickListener}
-          title="Continue with Google"
-          color="gray"
-        />
-      </View>
-
-      <Text numberOfLines={1}>
-        <Text> Don't have an account yet? </Text>
-        <Text
-          style={{ color: "red" }}
-          onPress={() => Linking.openURL("https://www.google.com/")}
-        >
-          Sign up
-        </Text>
-      </Text>
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
-}
-
-const label = StyleSheet.create({
-  container: {
-    alignItems: "flex-start",
-    justifyContent: "center",
-  },
-});
-
-const searchSection = StyleSheet.create({
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "#fff",
-});
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-
   input: {
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingBottom: 10,
-    paddingLeft: 0,
-    backgroundColor: "#fff",
-    color: "#424242",
+    width: 300,
+    height: 48,
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: "#D9D9D9",
+    borderWidth: 1,
+    borderColor: "#D9D9D9",
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  text2: {
+    fontSize: 32,
+    fontWeight: 600,
+    marginTop: 60,
+    position: "relative",
   },
 });
 
-const styles1 = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  setFontSizeOne: {
-    fontSize: 15, // Define font size here in Pixels
-  },
-  setFontSizeTwo: {
-    fontSize: 20, // Define font size here in Pixels
-  },
-  setFontSizeThree: {
-    fontSize: 25, // Define font size here in Pixels
-  },
-  setFontSizeFour: {
-    fontSize: 30, // Define font size here in Pixels
-  },
-});
+export default SignIn;
