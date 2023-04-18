3b8cf01dbf38b7f8ed9ad9a53fa223e801d95697
Khryx <khryxbergolafermat@gmail.com>
2023-04-18 14:37:20 +0800

    WIP

--- pages/Learn.js
+++ pages/Learn.js
@@ -1,14 +1,111 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text, Linking, Button, View, Image, TouchableOpacity } from "react-native";
 
-const Learn = () => {
+export default function Learn() {
   return (
-    <View>
-      <Text>Learn</Text>
+    <View style={{ paddingTop: 50, backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text style={{ fontSize: 25, paddingBottom: 15 }}>Learn</Text>
+        <Text style={{ fontSize: 20, paddingBottom: 10 }}>Section I</Text>
+
+        <View style={{ alignItems: "center", justifyContent: "center",
+            height: 290, width: 320,
+            borderRadius: 8, borderWidth: 0.5, borderColor: "gray",
+          }}>
+          <Image style={{ width: 106, height: 107 }}
+          source={require("../assets/BDLearnPic.png")}></Image>
+
+          <Text style={{fontSize: 17, paddingTop: 10, fontWeight: "bold"}}>
+            Blood Donation</Text>
+            <Text style={{fontSize: 10, paddingTop: 2, color:"gray"}}>
+            1/8 Units</Text>
+            <Image style={{ width: 290, height: 10, borderRadius: 8,}}
+          source={require("../assets/ProgressBar.png")}></Image>
+          <Text style={{fontSize: 10, paddingVertical: 10,paddingHorizontal: 55, textAlign: "center",}}>
+           Start with essential information and benefits on
+           blood donation.</Text>
+
+           <TouchableOpacity>
+              <View style={{ alignItems: "center", justifyContent: "center",
+            height: 30, width: 200, borderRadius: 8, backgroundColor: "#DD4438",
+              }}>
+                <Text style={{fontSize: 13, color: "#fff",}}>
+                  Start</Text> 
+              </View>
+           </TouchableOpacity>
+
+        </View>
+        
+
+        <View style={{ alignItems: "center", justifyContent: "center",
+            height: 20, width: 320, }}/>
+
+        <Text style={{ fontSize: 20, paddingBottom: 10 }}>Section II</Text>
+
+        <View style={{ alignItems: "center", justifyContent: "center",
+            height: 300, width: 320,
+            borderRadius: 8, borderWidth: 0.5, borderColor: "gray",
+          }}>
+          <Image style={{ width: 87, height: 117, transform: [{ scaleX: -1 }]}}
+          source={require("../assets/BDLearnPic.png")}></Image>
+
+          <Text style={{fontSize: 17, paddingTop: 10, fontWeight: "bold"}}>
+            Blood Request</Text>
+            <Text style={{fontSize: 10, paddingTop: 2, color:"gray"}}>
+            1/8 Units</Text>
+            <Image style={{ width: 290, height: 10, borderRadius: 8,}}
+          source={require("../assets/ProgressBar.png")}></Image>
+          <Text style={{fontSize: 10, paddingVertical: 10,paddingHorizontal: 55, textAlign: "center",}}>
+           Lorem Ipsum.</Text>
+
+           <TouchableOpacity>
+              <View style={{ alignItems: "center", justifyContent: "center",
+            height: 30, width: 200, borderRadius: 8, backgroundColor: "#DD4438",
+              }}>
+                <Text style={{fontSize: 13, color: "#fff",}}>
+                  Start</Text> 
+              </View>
+           </TouchableOpacity>
+
+        </View>
+
+
+      </View>
     </View>
-  )
+  );
 }
 
-export default Learn
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-const styles = StyleSheet.create({})
+  setFontSizeZero:{
+    fontSize: 10, // Define font size here in Pixels
+  },
+
+  setFontSizeOne: {
+    fontSize: 15, // Define font size here in Pixels
+  },
+  setFontSizeTwo: {
+    fontSize: 20, // Define font size here in Pixels
+    fontWeight: "bold",
+    letterSpacing: -0.4,
+  },
+  setFontSizeThree: {
+    fontSize: 25, // Define font size here in Pixels
+  },
+  setFontSizeFour: {
+    fontSize: 30, // Define font size here in Pixels
+  },
+  // Gray
+  setColor: {
+    color: "#434343",
+  },
+  // Red
+  setColor2: {
+    color: "#DD4438",
+  },
+});
