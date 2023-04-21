1ab50415c9f7bd268fef319fe2373ded63fe5198
Khryx <khryxbergolafermat@gmail.com>
2023-04-21 22:50:24 +0800

    Donor form UI

--- pages/donation/DonorForm.js
+++ pages/donation/DonorForm.js
@@ -4,6 +4,7 @@ import {
   TextInput,
   Button,
   Text,
+  ScrollView,
   DatePicker,
   TouchableOpacity,
   StyleSheet,
@@ -119,76 +120,147 @@ export default function DonorForm() {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
+    <ScrollView style={styles.opencontainer}>
+      <Text style={styles.mainheader}>Personal Information Confirmation:</Text>
+
+      <Text style={styles.subheader}>Confirm your personal information</Text>
+
+
+      <Text style={styles.inputheader}>First name</Text>
+      <TextInput style={styles.input}
         key="firstName"
-        placeholder="First Name"
         value={InputValues.FirstName}
         onChangeText={(value) => handleInputChange("FirstName", value)}
       />
-      <TextInput
+
+      <Text style={styles.inputheader}>Middle name</Text>
+      <TextInput style={styles.input}
         key="middleName"
-        placeholder="Middle Name"
         value={InputValues.MiddleName}
         onChangeText={(value) => handleInputChange("MiddleName", value)}
       />
-      <TextInput
+
+      <Text style={styles.inputheader}>Last name</Text>
+      <TextInput style={styles.input}
         key="LastName"
-        placeholder="Last Name"
         value={InputValues.LastName}
         onChangeText={(value) => handleInputChange("LastName", value)}
       />
-      <TextInput
+
+      <Text style={styles.inputheader}>Date of birth</Text> 
+      <TextInput style={styles.input}
         key="dateOfBirth"
-        placeholder="Date of Birth"
         value={InputValues.DateofBirth}
         onChangeText={(value) => handleInputChange("DateofBirth", value)}
       />
 
-      <TextInput
+      <Text style={styles.inputheader}>Country</Text>
+      <TextInput style={styles.input}
         key="country"
-        placeholder="Country"
         value={InputValues.Country}
         onChangeText={(value) => handleInputChange("Country", value)}
       />
-      <TextInput
+
+      <Text style={styles.inputheader}>City</Text>
+      <TextInput style={styles.input}
         key="city"
-        placeholder="City"
         value={InputValues.City}
         onChangeText={(value) => handleInputChange("City", value)}
       />
-      <TextInput
-        placeholder="Province"
+
+      <Text style={styles.inputheader}>Province</Text>
+      <TextInput style={styles.input}
         value={InputValues.Province}
         onChangeText={(value) => handleInputChange("Province", value)}
       />
-      <TextInput
-        placeholder="Street Address"
+
+      <Text style={styles.inputheader}>Street address</Text>
+      <TextInput style={styles.input}
         value={InputValues.Street_Address}
         onChangeText={(value) => handleInputChange("Street_Address", value)}
       />
-      <View>
-        <Text style={{ color: "#DD4438", fontSize: 12 }}>
+
+        <Text style={[styles.subheader,{color: "#DD4438", paddingTop: 20}]}>
           Contact Information
         </Text>
-        <TextInput
-          placeholder="Email"
+
+        <Text style={styles.inputheader}>Email</Text>
+        <TextInput style={styles.input}
           value={InputValues.Email}
           onChangeText={(value) => handleInputChange("Email", value)}
         />
-        <TextInput
-          placeholder="Phone Number"
+
+        <Text style={styles.inputheader}>Phone number</Text>
+        <TextInput style={styles.input}
           value={InputValues.Phone_Number}
           onChangeText={(value) => handleInputChange("Phone_Number", value)}
         />
+
+      <View style={{width: '100%', height: 30}}/>
+
+      <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+        {/*Back button*/}
+        <TouchableOpacity>
+          <View style={[styles.centerall,{borderColor: '#DD4438', borderWidth: 0.5, 
+                        borderRadius: 10, width: 100, height: 35}]}>
+            <Text style={{color: '#DD4438'}}>
+              Back</Text>
+            </View>
+        </TouchableOpacity>
+
+        {/*Next button*/}
+        <TouchableOpacity onPress={handleNextStep}>
+            <View style={[styles.centerall, {backgroundColor: '#DD4438',
+                        borderRadius: 10, width: 200, height: 35}]}>
+            <Text style={{color: "white"}}>
+              Next question</Text>
+            </View>
+        </TouchableOpacity>
       </View>
 
-      <Button title="Next" onPress={handleNextStep} />
-    </View>
+      <View style={{width: '100%', height: 110}}/>
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  opencontainer:{
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 65,
+    backgroundColor: '#fff',
+  },
+
+  centerall:{
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  input:{
+    width: '100%',
+    height: 40,
+    backgroundColor: '#D3D3D3',
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+
+  mainheader:{
+    fontSize: 25,
+    fontWeight: "600",
+    paddingBottom: 12,
+  },
+
+  subheader:{
+    fontSize: 15,
+    paddingBottom: 12,
+  },
+
+  inputheader:{
+    fontSize: 15,
+    paddingBottom: 2,
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
