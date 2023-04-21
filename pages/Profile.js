6e15c7c53663417f35cacc76ae8360ee9d94dbed
Craven <KrenntKB@yahoo.com>
2023-04-21 11:11:34 +0800

    -User Profile not Fixed yet.

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,134 +1,287 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, Linking, Button, ScrollView,View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+  Button,
+  ScrollView,
+  View,
+  Image,
+} from "react-native";
 import AccountSetting from "./AccountSetting";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import firebase from "firebase/compat";
 
 export default function Profile() {
   const navigation = useNavigation();
   const route = useRoute();
   return (
-    <ScrollView style={{ paddingTop: 50, flex: 1, }}>
+    <ScrollView style={{ paddingTop: 50, flex: 1 }}>
       <View style={{ paddingHorizontal: 20 }}>
-
-      <View style={{width: "100%", justifyContent: "space-between",
-            alignItems: "baseline", flexDirection: "row",}}>
+        <View
+          style={{
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "baseline",
+            flexDirection: "row",
+          }}
+        >
           <TouchableOpacity onPress={() => navigation.navigate(AccountSetting)}>
-          <View>
-          <Image
-          style={{ width: 40, height: 40 }}
-          source={require("../assets/BackButton.png")}/>
-          </View> 
+            <View>
+              <Image
+                style={{ width: 40, height: 40 }}
+                source={require("../assets/BackButton.png")}
+              />
+            </View>
           </TouchableOpacity>
 
-          <View style={{ alignItems: "flex-end",}}>
-                <Text style={{fontSize: 25, paddingBottom: 10, color:"#E1E1E1"}}>User Profile</Text>
-            </View>
-
+          <View style={{ alignItems: "flex-end" }}>
+            <Text style={{ fontSize: 25, paddingBottom: 10, color: "#E1E1E1" }}>
+              User Profile
+            </Text>
+          </View>
         </View>
 
         <View
           style={{
-            alignItems: "center", justifyContent: "center",}}>
-            <View
-          style={{
-            height: 100, width: 100, borderRadius: 100, backgroundColor: "gray", marginTop: 10,
-          }}/>
-          <View style={{height: 22, width: 90, backgroundColor: "#DD4438",
-                            borderRadius: 5, marginTop: 10}}>
-                    <Text style={{textAlign:"center", fontSize: 12, 
-                                color: "white",marginVertical: 2}}>
-                        Blood Type: </Text>
-                </View>
-          <Text style={[styles1.setFontSizeThree, {paddingVertical: 5,fontWeight:"bold"}]}>
-            First Name Last Name</Text>
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              height: 100,
+              width: 100,
+              borderRadius: 100,
+              backgroundColor: "gray",
+              marginTop: 10,
+            }}
+          />
+          <View
+            style={{
+              height: 22,
+              width: 90,
+              backgroundColor: "#DD4438",
+              borderRadius: 5,
+              marginTop: 10,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 12,
+                color: "white",
+                marginVertical: 2,
+              }}
+            >
+              Blood Type:{" "}
+            </Text>
+          </View>
+          <Text
+            style={[
+              styles1.setFontSizeThree,
+              { paddingVertical: 5, fontWeight: "bold" },
+            ]}
+          >
+            {firebase.auth().currentUser.email}
+          </Text>
           <Text style={styles1.setFontSizeOne}>Location</Text>
         </View>
-        
-        <View>
-        <Text style={[styles1.setFontSizeTwo,{paddingBottom: 5, paddingTop: 10}]}>Badges</Text>
-            <View style={{flexDirection: "row"}}>
-                <View style={{height: 50, width: 50, backgroundColor: "gray",
-                borderRadius: 50, marginHorizontal: 5}}/>
-                <View style={{height: 50, width: 50, backgroundColor: "gray",
-                borderRadius: 50, marginHorizontal: 5}}/>
-                <View style={{height: 50, width: 50, backgroundColor: "gray",
-                borderRadius: 50, marginHorizontal: 5}}/>
-            </View>
 
+        <View>
+          <Text
+            style={[
+              styles1.setFontSizeTwo,
+              { paddingBottom: 5, paddingTop: 10 },
+            ]}
+          >
+            Badges
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                backgroundColor: "gray",
+                borderRadius: 50,
+                marginHorizontal: 5,
+              }}
+            />
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                backgroundColor: "gray",
+                borderRadius: 50,
+                marginHorizontal: 5,
+              }}
+            />
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                backgroundColor: "gray",
+                borderRadius: 50,
+                marginHorizontal: 5,
+              }}
+            />
+          </View>
         </View>
-        
-        <View style={{height: 20, width: "100%",}}/>
+
+        <View style={{ height: 20, width: "100%" }} />
 
         <Text style={{ fontSize: 20, paddingBottom: 10 }}>
           Your Past Donations
         </Text>
       </View>
 
-      <View style={{ flexDirection: "column", }}>
-        <View style={{ width: "100%", height: 100, borderWidth: 0.3,
-                      flexDirection: "row",}}>
-          <View style={{height: 80, width: 80, borderRadius: 10, margin: 10,
-                      backgroundColor:"gray", paddingHorizontal: 20 }}/>
-          <View style={{flexDirection: "column", justifyContent: "center", paddingHorizontal: 20}}>
+      <View style={{ flexDirection: "column" }}>
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderWidth: 0.3,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              height: 80,
+              width: 80,
+              borderRadius: 10,
+              margin: 10,
+              backgroundColor: "gray",
+              paddingHorizontal: 20,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              paddingHorizontal: 20,
+            }}
+          >
             <Text style={styles1.setFontSizeOne}>Your Past Donation</Text>
             <Text style={styles1.setFontSizeZero}>Details</Text>
             <Text style={styles1.setFontSizeZero}>Details</Text>
           </View>
-          
         </View>
         <View style={{ width: "100%", height: 20 }}></View>
-        <View style={{ width: "100%", height: 100, borderWidth: 0.3,
-                      flexDirection: "row",}}>
-          <View style={{height: 80, width: 80, borderRadius: 10, margin: 10,
-                      backgroundColor:"gray", paddingHorizontal: 20 }}/>
-          <View style={{flexDirection: "column", justifyContent: "center", paddingHorizontal: 20}}>
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderWidth: 0.3,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              height: 80,
+              width: 80,
+              borderRadius: 10,
+              margin: 10,
+              backgroundColor: "gray",
+              paddingHorizontal: 20,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              paddingHorizontal: 20,
+            }}
+          >
             <Text style={styles1.setFontSizeOne}>Your Past Donation</Text>
             <Text style={styles1.setFontSizeZero}>Details</Text>
             <Text style={styles1.setFontSizeZero}>Details</Text>
           </View>
-          
         </View>
       </View>
-      
-      <Text style={{ fontSize: 20, paddingBottom: 10, paddingTop: 30, 
-                  paddingHorizontal: 20}}>
-          Your Past Requests
+
+      <Text
+        style={{
+          fontSize: 20,
+          paddingBottom: 10,
+          paddingTop: 30,
+          paddingHorizontal: 20,
+        }}
+      >
+        Your Past Requests
+      </Text>
+
+      <View style={{ flexDirection: "column" }}>
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderWidth: 0.3,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              height: 80,
+              width: 80,
+              borderRadius: 10,
+              margin: 10,
+              backgroundColor: "gray",
+              paddingHorizontal: 20,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text style={styles1.setFontSizeOne}>Your Past Requests</Text>
+            <Text style={styles1.setFontSizeZero}>Details</Text>
+            <Text style={styles1.setFontSizeZero}>Details</Text>
+          </View>
+        </View>
+        <View style={{ width: "100%", height: 20 }}></View>
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderWidth: 0.3,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              height: 80,
+              width: 80,
+              borderRadius: 10,
+              margin: 10,
+              backgroundColor: "gray",
+              paddingHorizontal: 20,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text style={styles1.setFontSizeOne}>Your Past Requests</Text>
+            <Text style={styles1.setFontSizeZero}>Details</Text>
+            <Text style={styles1.setFontSizeZero}>Details</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ alignItems: "center", paddingVertical: 20 }}>
+        <Text numberOfLines={1}>
+          <Text>Can't find your request history? </Text>
+          <Text style={{ color: "#2E5C65" }}>Contact us</Text>
         </Text>
-
-      <View style={{ flexDirection: "column", }}>
-        <View style={{ width: "100%", height: 100, borderWidth: 0.3,
-                      flexDirection: "row",}}>
-          <View style={{height: 80, width: 80, borderRadius: 10, margin: 10,
-                      backgroundColor:"gray", paddingHorizontal: 20 }}/>
-          <View style={{flexDirection: "column", justifyContent: "center", paddingHorizontal: 20}}>
-            <Text style={styles1.setFontSizeOne}>Your Past Requests</Text>
-            <Text style={styles1.setFontSizeZero}>Details</Text>
-            <Text style={styles1.setFontSizeZero}>Details</Text>
-          </View>
-          
-        </View>
-        <View style={{ width: "100%", height: 20 }}></View>
-        <View style={{ width: "100%", height: 100, borderWidth: 0.3,
-                      flexDirection: "row",}}>
-          <View style={{height: 80, width: 80, borderRadius: 10, margin: 10,
-                      backgroundColor:"gray", paddingHorizontal: 20 }}/>
-          <View style={{flexDirection: "column", justifyContent: "center", paddingHorizontal: 20}}>
-            <Text style={styles1.setFontSizeOne}>Your Past Requests</Text>
-            <Text style={styles1.setFontSizeZero}>Details</Text>
-            <Text style={styles1.setFontSizeZero}>Details</Text>
-          </View>
-          
-        </View>
       </View>
-
-      <View style={{alignItems: "center", paddingVertical: 20}}>
-      <Text numberOfLines={1}>
-            <Text>Can't find your request history? </Text>
-            <Text style={{ color: "#2E5C65" }}>Contact us</Text>
-          </Text>
-      </View>
-      <View style={{ width: "100%", height: 120}}></View>
-
+      <View style={{ width: "100%", height: 120 }}></View>
     </ScrollView>
   );
 }
@@ -140,7 +293,7 @@ const styles1 = StyleSheet.create({
     alignItems: "center",
   },
 
-  setFontSizeZero:{
+  setFontSizeZero: {
     fontSize: 10, // Define font size here in Pixels
   },
 
--- pages/SignUp.js
+++ pages/SignUp.js
@@ -8,12 +8,15 @@ import {
   Alert,
   TouchableOpacity,
 } from "react-native";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 
 const SignUp = () => {
   const navigation = useNavigation();
   const route = useRoute();
+
+  const RequestCollectionRef = collection(db, "User");
 
   const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
   const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
@@ -22,6 +25,11 @@ const SignUp = () => {
     email: "",
     password: "",
     confirmPassword: "",
+    FirstName: "",
+    LastName: "",
+    MiddleName: "",
+    step: 1,
+    UserID: "",
   });
 
   const handleInputChange = (name, value) => {
@@ -52,9 +60,64 @@ const SignUp = () => {
       .then((userCredential) => {
         const user = userCredential.user;
         Alert.alert("Success", "Account created successfully");
-        navigation.navigate("SignIn");
+        db.collection(`Users/${user.uid}`)
+          .doc("profile")
+          .set({
+            FirstName: inputValues.FirstName,
+            LastName: inputValues.LastName,
+            MiddleName: inputValues.MiddleName,
+            Email: inputValues.email,
+            Password: inputValues.password,
+            UserID: user.uid,
+          })
+          .then(() => {
+            console.log("User data added to Firestore.");
+            navigation.navigate("SignIn");
+          })
+          .catch((error) => {
+            console.error("Error adding user data to Firestore:", error);
+          });
       })
-      .catch((error) => alert(error.message));
+      .catch((error) => {
+        console.error("Error creating account:", error);
+        Alert.alert("Error", error.message);
+      });
+  };
+
+  const handleNextStep = () => {
+    if (inputValues.step === 1) {
+      if (!EMAIL_REGEX.test(inputValues.email)) {
+        Alert.alert("Invalid Email", "Please enter a valid email address");
+        return;
+      }
+      if (!PASSWORD_REGEX.test(inputValues.password)) {
+        Alert.alert(
+          "Invalid Password",
+          "Password must be at least 8 characters long and contain at least one letter and one number"
+        );
+        return;
+      }
+      if (inputValues.password !== inputValues.confirmPassword) {
+        Alert.alert(
+          "Passwords do not match",
+          "Please enter matching passwords"
+        );
+        return;
+      }
+      setInputValues({
+        ...inputValues,
+        step: 2,
+      });
+    }
+  };
+
+  const handlePreviousStep = () => {
+    if (inputValues.step === 2) {
+      setInputValues({
+        ...inputValues,
+        step: 1,
+      });
+    }
   };
 
   return (
@@ -67,53 +130,116 @@ const SignUp = () => {
           Account.
         </Text>
       </View>
-
       {/*Input account details*/}
-      {/*Email*/}
-      <View>
-        <Text style={styles.text1}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder=""
-          onChangeText={(text) => handleInputChange("email", text)}
-          value={inputValues.email}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-      </View>
+      {inputValues.step === 1 && (
+        <>
+          {/*Email*/}
+          <View>
+            <Text style={styles.text1}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=""
+              onChangeText={(text) => handleInputChange("email", text)}
+              value={inputValues.email}
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
 
-      {/*Password*/}
-      <View>
-        <Text style={styles.text1}>Password</Text>
-        <TextInput
-          style={styles.input}
-          placeholder=""
-          onChangeText={(text) => handleInputChange("password", text)}
-          value={inputValues.password}
-          secureTextEntry={true}
-        />
-      </View>
+          {/*Password*/}
+          <View>
+            <Text style={styles.text1}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=""
+              onChangeText={(text) => handleInputChange("password", text)}
+              value={inputValues.password}
+              secureTextEntry={true}
+            />
+          </View>
 
-      {/*Confirm Password*/}
-      <View>
-        <Text style={styles.text1}>Confirm Password</Text>
-        <TextInput
-          style={[styles.input, { marginBottom: 20 }]}
-          placeholder=""
-          onChangeText={(text) => handleInputChange("confirmPassword", text)}
-          value={inputValues.confirmPassword}
-          secureTextEntry={true}
-        />
-      </View>
+          {/*Confirm Password*/}
+          <View>
+            <Text style={styles.text1}>Confirm Password</Text>
+            <TextInput
+              style={[styles.input, { marginBottom: 20 }]}
+              placeholder=""
+              onChangeText={(text) =>
+                handleInputChange("confirmPassword", text)
+              }
+              value={inputValues.confirmPassword}
+              secureTextEntry={true}
+            />
+          </View>
 
-      {/*Next Button*/}
-      <TouchableOpacity onPress={handleSubmit}>
-        <View style={[styles.centerall, styles.nextbutton]}>
-          <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
-            Next
-          </Text>
-        </View>
-      </TouchableOpacity>
+          {/*Next Button*/}
+          <TouchableOpacity onPress={handleNextStep}>
+            <View style={[styles.centerall, styles.nextbutton]}>
+              <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
+                Next
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </>
+      )}
+
+      {inputValues.step === 2 && (
+        <>
+          {/*First Name*/}
+          <View>
+            <Text style={styles.text1}>First Name:</Text>
+            <TextInput
+              style={[styles.input, { marginBottom: 20 }]}
+              placeholder=""
+              onChangeText={(text) => handleInputChange("FirstName", text)}
+              value={inputValues.FirstName}
+            />
+          </View>
+
+          {/*Last Name*/}
+          <View>
+            <Text style={styles.text1}>Last Name:</Text>
+            <TextInput
+              style={[styles.input, { marginBottom: 20 }]}
+              placeholder=""
+              onChangeText={(text) => handleInputChange("LastName", text)}
+              value={inputValues.LastName}
+            />
+          </View>
+
+          {/*Last Name*/}
+          <View>
+            <Text style={styles.text1}>Middle Name:</Text>
+            <TextInput
+              style={[styles.input, { marginBottom: 20 }]}
+              placeholder=""
+              onChangeText={(text) => handleInputChange("MiddleName", text)}
+              value={inputValues.MiddleName}
+            />
+          </View>
+
+          {/*Back Button*/}
+          <TouchableOpacity
+            onPress={handlePreviousStep}
+            style={{ marginBottom: 20 }}
+          >
+            <View style={[styles.centerall, styles.nextbutton]}>
+              <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
+                Back
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          {/*Sign Up Button*/}
+          <TouchableOpacity onPress={handleSubmit}>
+            <View style={[styles.centerall, styles.nextbutton]}>
+              <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
+                Sign Up
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </>
+      )}
 
       {/*Already have an account?*/}
       <View style={[styles.centerall, { paddingVertical: 10 }]}>
